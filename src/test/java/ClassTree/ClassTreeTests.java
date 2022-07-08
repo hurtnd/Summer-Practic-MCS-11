1222ae839023d754380a2880b60838204f022990
hurtnd <57873165+hurtnd@users.noreply.github.com>
2022-07-08 14:11:26 +0300

    small edits

--- src/test/java/ClassTree/ClassTreeTests.java
+++ src/test/java/ClassTree/ClassTreeTests.java
@@ -1,6 +1,5 @@
 package ClassTree;
 
-import ClassTree.ClassTree;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,69 +8,69 @@ public class ClassTreeTests {
 
     @Test
     void createNode(){
-        ClassTree node = new ClassTree("Корень");
-        assertEquals("Корень", node.getName());
+        ClassTree node = new ClassTree("Root");
+        assertEquals("Root", node.getName());
     }
 
     @Test
     void addNode(){
-        ClassTree root = new ClassTree("Корень");
-        ClassTree child = new ClassTree("Лист");
+        ClassTree root = new ClassTree("Root");
+        ClassTree child = new ClassTree("Leaf");
         root.add(child);
         assertEquals(1, root.getChild().size());
-        assertEquals("Лист", root.getChild().get(0).getName());
+        assertEquals("Leaf", root.getChild().get(0).getName());
     }
 
     @Test
     void deleteNode()
     {
-        ClassTree root = new ClassTree("Корень");
-        ClassTree child = new ClassTree("Лист");
+        ClassTree root = new ClassTree("Root");
+        ClassTree child = new ClassTree("Leaf");
         root.add(child);
         assertEquals(1, root.getChild().size());
-        assertEquals("Лист", root.getChild().get(0).getName());
-        child.delete("Лист");
+        assertEquals("Leaf", root.getChild().get(0).getName());
+        child.delete("Leaf");
     }
 
     @Test
     void deleteChild()
     {
-        ClassTree root = new ClassTree("Корень");
-        ClassTree child = new ClassTree("Лист");
-        ClassTree parent = new ClassTree("Родитель");
+        ClassTree root = new ClassTree("Root");
+        ClassTree child = new ClassTree("Leaf");
+        ClassTree parent = new ClassTree("Parent");
         root.add(child);
         root.add(parent);
         assertEquals(2, root.getChild().size());
-        assertEquals("Лист", root.getChild().get(0).getName());
-        assertEquals("Родитель", root.getChild().get(1).getName());
-        root.deleteOC("Корень");
+        assertEquals("Leaf", root.getChild().get(0).getName());
+        assertEquals("Parent", root.getChild().get(1).getName());
+        root.deleteOC("Root");
     }
 
     @Test
     void search()
     {
-        ClassTree root = new ClassTree("Корень");
-        ClassTree child = new ClassTree("Лист");
-        ClassTree parent = new ClassTree("Родитель");
+        ClassTree root = new ClassTree("Root");
+        ClassTree child = new ClassTree("Leaf");
+        ClassTree parent = new ClassTree("Parent");
         root.add(child);
         root.add(parent);
         assertEquals(2, root.getChild().size());
-        assertEquals("Лист", root.getChild().get(0).getName());
-        assertEquals("Родитель", root.getChild().get(1).getName());
-        root.search("Родитель");
+        assertEquals("Leaf", root.getChild().get(0).getName());
+        assertEquals("Parent", root.getChild().get(1).getName());
+        root.search("Parent");
     }
 
     @Test
     void rename()
     {
-        ClassTree root = new ClassTree("Корень");
-        ClassTree child = new ClassTree("Лист");
-        ClassTree parent = new ClassTree("Родитель");
+        ClassTree root = new ClassTree("Root");
+        ClassTree child = new ClassTree("Leaf");
+        ClassTree parent = new ClassTree("Parent");
         root.add(child);
         root.add(parent);
         assertEquals(2, root.getChild().size());
-        assertEquals("Лист", root.getChild().get(0).getName());
-        assertEquals("Родитель", root.getChild().get(1).getName());
-        root.rename("Родитель", "Папа");
+        assertEquals("Leaf", root.getChild().get(0).getName());
+        assertEquals("Parent", root.getChild().get(1).getName());
+        root.rename("Parent", "Friend");
     }
 }
